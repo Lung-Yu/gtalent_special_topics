2d0a732f643b01921f24d2d4ea6165bceb3da25f
tygrus <workfile975@gmail.com>
2026-04-23 21:43:02 +0800

    feat: 更新 HelloController 和 hello.html，新增用戶名稱顯示及產品列表

--- apps/helloworld/src/main/java/com/gtalent/helloworld/controller/HelloController.java
+++ apps/helloworld/src/main/java/com/gtalent/helloworld/controller/HelloController.java
@@ -1,15 +1,32 @@
 package com.gtalent.helloworld.controller;
 
+import com.gtalent.helloworld.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.List;
+import java.util.Random;
 
 @Controller
 public class HelloController {
-    
+
+    private static final List<String> NAMES = List.of(
+            "Alice", "Bob", "Charlie", "Diana", "Edward",
+            "Fiona", "George", "Hannah", "Ivan", "Julia"
+    );
+
+    private static final Random RANDOM = new Random();
+
+    @Autowired
+    private ProductService productService;
 
     @GetMapping("/hello")
-    public String hello(){
+    public String hello(Model model) {
+        String userName = NAMES.get(RANDOM.nextInt(NAMES.size()));
+        model.addAttribute("userName", userName);
+        model.addAttribute("allProducts", productService.getProducts());
         return "hello";
     }
 
